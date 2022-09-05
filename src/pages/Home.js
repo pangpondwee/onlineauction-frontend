9513237fc5d243048fe67b3f644745a4958197ef
BardRoll <thotsapol.nine@gmail.com>
2022-09-05 16:02:13 +0700

    add row card flexbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,13 @@ const Home = () =>{
 	return (
 		<>
 		<h1>Home</h1>
+		<div className="test">
 		<AuctionCard name="a"/>
+		<AuctionCard name="b"/>
+		<AuctionCard name="c"/>
+		<AuctionCard name="d"/>
+		<AuctionCard name="e"/>
+		</div>
 		</>
 	)
 }
