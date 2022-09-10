8d44b8e2855185291c7b749b2b298ee30456a380
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-10 22:01:59 +0700

    Add payment route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from './pages/Layout'
 import Home from './pages/Home'
 import NoPage from './pages/NoPage'
 import PlaceAuction from './pages/PlaceAuction'
+import Payment from './pages/Payment'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="place_auction" element={<PlaceAuction />} />
+          <Route path="payment" element={<Payment />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
