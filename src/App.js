2d6aeeb4ad60a3386b296cf5e96d0ec6029ddce5
Ham-Libelta <ham.chaninchai@gmail.com>
2022-09-11 20:21:42 +0700

    FAQ Page Finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // Pages
 import Layout from "./pages/Layout";
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -6,9 +6,12 @@ const FAQ = () => {
             <h3 className="faq-title">Frequently asked questions</h3>
             <div className="Q1">
                 <div className="Q1-1">
-                    <h4>Q: What is Closed/Open bid?</h4>
+                    <h4 className="question">Q: What is Closed/Open bid?</h4>
+                </div>
+                <div className="Q1-2">
+                    <h4>A:</h4>
                     <div>
-                        A: Closed Bid : User ที่ไม่ใช่ Bidder ในการร่วมประมูลครั้งนี้จะเห็นแค่ Current Price Bid และราคาของสินค้าเริ่มต้น
+                        Closed Bid : User ที่ไม่ใช่ Bidder ในการร่วมประมูลครั้งนี้จะเห็นแค่ Current Price Bid และราคาของสินค้าเริ่มต้น
                         <ul>
                             <li>ชื่อ และ Rating ของ Auctioneer</li>
                             <li>Description ของสินค้า</li>
@@ -22,11 +25,41 @@ const FAQ = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="Q1-2">
-                    <h4>Q: What is expected price?</h4>
+            </div>
+            <div className="Q2">
+                <div className="Q2-1">
+                    <h4 className="question">Q: What is expected price?</h4>
+                </div>
+                <div className="Q2-2">
+                    <h4>A:</h4>
                     <div>
-                        A: ถ้าราคาสูงสุดปัจจุบันสูงกว่าหรือเท่ากับ Expected Price ของ Auctioneer ระบบจะส่งแจ้งเตือนไปยังคนที่ Follow และ Favorite ไว้ จากนั้นจะลดระยะเวลาในการประมูลเหลือเพียง 1 ชั่วโมงและนับถอยหลังเข้าสู่ระบบ Five Minute System
+                        ถ้าราคาสูงสุดปัจจุบันสูงกว่าหรือเท่ากับ Expected Price ของ Auctioneer ระบบจะส่งแจ้งเตือนไปยังคนที่ Follow และ Favorite ไว้ จากนั้นจะลดระยะเวลาในการประมูลเหลือเพียง 1 ชั่วโมงและนับถอยหลังเข้าสู่ระบบ Five Minute System
                     </div>
+                </div>
+            </div>
+            <div className="Q3">
+                <div className="Q3-1">
+                    <h4 className="question">Q: What is minimum bid step?</h4>
+                </div>
+                <div className="Q3-2">
+                    <table className="bid-step-table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ช่วงราคาสินค้า</th>
+                                <th scope="col">Minimum bid price</th>
+                            </tr>
+                        </thead>
+                        <tbody className=" table-body table-group-divsider">
+                            <tr>
+                                <th scope="row">น้อยกว่าหรือเท่ากับ 5000 บาท</th>
+                                <td>Bid เพิ่มขั้นต่ำ 50 บาท</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">มากกว่า 5000 บาท</th>
+                                <td>ราคาเริ่มต้น / 100 แล้วปัดขึ้นทุกกรณีให้เหลือเลขเพียงหลักเดียว</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
