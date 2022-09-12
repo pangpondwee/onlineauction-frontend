5f0e847d2c1a7f2224c3e1234e531fe4efe1f5b7
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-12 16:45:04 +0700

    Create billing info page with item card

---
+++ src/pages/BillingInfo.js
@@ -0,0 +1,12 @@
+import ItemCard from '../components/ItemCard'
+
+const BillingInfo = () => {
+  return (
+    <div class="billing-info-page">
+      <ItemCard></ItemCard>
+      <div class="billing-info"></div>
+    </div>
+  )
+}
+
+export default BillingInfo
