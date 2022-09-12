3d3fc7e8a0d552d69b2f33b5a1a77c2b13dea54a
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-12 13:33:21 +0700

    Create shipping pages and create shipping addr box

---
+++ src/pages/Shipping.js
@@ -0,0 +1,21 @@
+const Shipping = () => {
+  return (
+    <div>
+      <h1 class="header">Shipping</h1>
+      <form>
+        <div class="form">
+          <div class="formHeading1">SHIPPING ADDRESS</div>
+          <div class="subForm col-5">
+            <div class="address-box">
+              <div>Pakkapol Kong</div>
+              <div>50 Ngamwongwan Rd, Chatuchak Bangkok 10900 Thailand</div>
+              <div>069-696-9696</div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Shipping
