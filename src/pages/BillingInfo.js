75471ae77db0df608dfa725146e58f9a830d2312
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-12 18:19:17 +0700

    Edit billing info pages

--- src/pages/BillingInfo.js
+++ src/pages/BillingInfo.js
@@ -1,10 +1,21 @@
 import ItemCard from '../components/ItemCard'
+import AddressBox from '../components/Address-box'
+import TrackingCard from '../components/TrackingCard'
+import SummaryCard from '../components/SummaryCard'
 
 const BillingInfo = () => {
   return (
     <div class="billing-info-page">
       <ItemCard></ItemCard>
-      <div class="billing-info"></div>
+      <div class="billing-info">
+        <div class="billing-info-subcard vertical-align-card">
+          <AddressBox></AddressBox>
+          <SummaryCard></SummaryCard>
+        </div>
+        <div class="billing-info-subcard">
+          <TrackingCard></TrackingCard>
+        </div>
+      </div>
     </div>
   )
 }
