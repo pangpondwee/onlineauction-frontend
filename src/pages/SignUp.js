41a08f6aeffeaa4e53d962b58eaf0bf82cb09e00
kong-wep <pakkapol.20100@gmail.com>
2022-09-19 13:10:50 +0700

    Moved login to Signup.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,47 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment,useRef } from "react";
 
 import classes from "../css/SignUp.module.css";
 import Card from "../components/Card";
 
 import pic1 from "../pictures/loginpic.jpg";
+import { Link,useOutletContext,useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  let navigate = useNavigate();
+  const [loggedIn, setloggedIn] = useOutletContext();
+  const email = useRef();
+  const password = useRef();
+  const confirmPassword = useRef();
+  const onSignInSubmit = (e) => {
+    fetch("http://13.250.98.9/api/user/signin", {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({
+        email: email.current.value.trim(),
+        password: password.current.value.trim()
+      })
+    }).then(res => {
+      return res.json();
+    }).then(res_data=>{
+      if(res_data.status == "success"){
+        const data = res_data.data.user;
+        localStorage.setItem("displayName",data.displayName)
+        setloggedIn(true);
+        navigate("/");
+      }
+      else{
+        throw new Error(res_data.message);
+      }
+    }).catch((error)=>{
+      console.log(error)
+    })
+    e.preventDefault()
+  }
+
   return (
     <Fragment>
       <h1>Anything You Want, At A Super Satisfying Price!</h1>
@@ -14,10 +49,10 @@ const SignUp = () => {
       <div className={classes.SignUp}>
         <img className={classes.loginimg} src={pic1} />
         <Card className={classes.input}>
-          <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item" role="presentation">
               <button
-                class="nav-link active"
+                className="nav-link active"
                 id="SignUp-tab"
                 data-bs-toggle="tab"
                 data-bs-target="#SignUp-tab-pane"
@@ -29,9 +64,9 @@ const SignUp = () => {
                 Sign Up
               </button>
             </li>
-            <li class="nav-item" role="presentation">
+            <li className="nav-item" role="presentation">
               <button
-                class="nav-link"
+                className="nav-link"
                 id="SignIn-tab"
                 data-bs-toggle="tab"
                 data-bs-target="#SignIn-tab-pane"
@@ -44,15 +79,15 @@ const SignUp = () => {
               </button>
             </li>
           </ul>
-          <div class="tab-content" id="myTabContent">
+          <div className="tab-content" id="myTabContent">
             <div
-              class="tab-pane fade show active"
+              className="tab-pane fade show active"
               id="SignUp-tab-pane"
               role="tabpanel"
               aria-labelledby="SignUp-tab"
-              tabindex="0"
+              tabIndex={"0"}
             >
-              <div className={classes.Form}>
+              <form className={classes.Form}>
                 <label htmlFor="displayname">Display Name</label>
                 <input
                   id="displayname"
@@ -60,37 +95,37 @@ const SignUp = () => {
                   placeholder="Display Name"
                 />
                 <label htmlFor="email">Email</label>
-                <input id="email" type="text" placeholder="Email" />
+                <input id="email" type="text" placeholder="Email" ref={email}/>
                 <label htmlFor="password">Password</label>
-                <input id="password" type="text" placeholder="Password" />
-                <label htmlFor="confirmpassword">Confirm Password</label>
+                <input id="password" type="text" placeholder="Password" ref={password}/>
+                <label htmlFor="confirmpassword" ref={confirmPassword}>Confirm Password</label>
                 <input
                   id="confirmpassword"
                   type="text"
                   placeholder="Confirm Password"
                 />
                 <div>
-                  <button className={classes.button}>Sign Up</button>
+                  <input type="submit" className={classes.button} value="Sign Up"/>
                 </div>
-              </div>
+              </form>
             </div>
             <div
-              class="tab-pane fade"
+              className="tab-pane fade"
               id="SignIn-tab-pane"
               role="tabpanel"
               aria-labelledby="SignIn-tab"
-              tabindex="0"
+              tabIndex={"1"}
             >
-              <div className={classes.Form}>
+              <form className={classes.Form} action="" onSubmit={onSignInSubmit}>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="text" placeholder="Email" />
+                <input id="email" type="text" placeholder="Email" ref={email}/>
                 <label htmlFor="password">Password</label>
-                <input id="password" type="text" placeholder="Password" />
+                <input id="password" type="password" placeholder="Password" ref={password}/>
                 <Link to={"#"}>Forgot Password?</Link>
                 <div>
-                  <button className={classes.button}>Sign In</button>
+                  <input type="submit" className={classes.button} value="Sign In"/>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </Card>
