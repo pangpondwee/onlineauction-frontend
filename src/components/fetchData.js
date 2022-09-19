854ab50e8bd7969e2babad62639ac4a7cf9c4286
kong-wep <pakkapol.20100@gmail.com>
2022-09-19 16:44:33 +0700

    Added postData function

--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -34,23 +34,7 @@ export const postData = async (url,data)=>{
 		body: data
 	})
 	.then((res)=>{
-		// if(!res.ok){
-		// 	throw new Error(res.status); // Error 400 500
-		// }
-		// else{
-			return res.json();
-		// }
-	})
-	.then(res_data=>{
-		if(res_data.status == "success"){
-			return ["success",res_data.data];
-		}
-		else{
-			throw new Error(res_data.message);
-		}
-	})
-	.catch(error=>{
-		return ["fail",{message : error.message}]
+		return res.json();
 	})
 }
 export default fetchData;
