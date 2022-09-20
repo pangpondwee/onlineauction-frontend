fa2277343272fc8ae3448dfca03c18e3a15fc27b
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-20 16:08:39 +0700

    Add props and edit some class name

--- src/components/PaymentSummaryCard.js
+++ src/components/PaymentSummaryCard.js
@@ -1,16 +1,16 @@
 import nintendo_switch from '../pictures/nintendo-switch.jpeg'
-const PaymentSummaryCard = () => {
+const PaymentSummaryCard = (props) => {
   return (
     <div class="grey-box payment-summary-card">
-      <div>Summary</div>
-      <div>Nintendo Switch</div>
+      <h3>Summary</h3>
+      <div>{props.itemName}</div>
       <div class="center-pic">
         <img src={nintendo_switch} alt="nintendo switch"></img>
       </div>
 
-      <div>Auctioneer: Kong Pakkapol</div>
+      <div>Auctioneer: {props.auctioneerName}</div>
       <hr></hr>
-      <div>Price: 2000 Baht</div>
+      <h3>Price: 2000 Baht</h3>
     </div>
   )
 }
