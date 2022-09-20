c968affe0e897a3c1f0aaa977a59b3705974b307
ZisZtR <zisztr@gmail.com>
2022-09-20 10:41:21 +0700

    commit before pull

--- src/pages/AccountEdit.js
+++ src/pages/AccountEdit.js
@@ -13,24 +13,24 @@ const AccountEdit = () =>{
                     </div>
                     <div>
                         <div>
-                            <h5>Name</h5>
-                            <input type="text" className="input-editing" placeholder="Peeranut Srisuthangkul"/>
+                            <label><h5>Name</h5></label>
+                            <input type="text" for="Name" className="input-editing" placeholder="Peeranut Srisuthangkul"/>
                         </div>
                         <div>
-                            <h5>Email</h5>
-                            <input type="text" className="input-editing" placeholder="peeranut.sri@ku.th"/>
+                            <label><h5>Email</h5></label>
+                            <input type="text" for="Email" className="input-editing" placeholder="peeranut.sri@ku.th"/>
                         </div>
                         <div>
-                            <h5>Phone Number</h5>
-                            <input type="text" className="input-editing" placeholder="06200000000"/>
+                            <label><h5>Phone Number</h5></label>
+                            <input type="text" for="Phone Number" className="input-editing" placeholder="06200000000"/>
                         </div>
                         <div>
-                            <h5>Address</h5>
-                            <textarea type="text" className="input-editing" placeholder="4412 Matlock Rd #200 Arlington Texas United States 76018"/>
+                            <label><h5>Address</h5></label>
+                            <textarea type="text" for="Address" className="input-editing" placeholder="4412 Matlock Rd #200 Arlington Texas United States 76018"/>
                         </div>
                         <div>
-                            <h5>Description</h5>
-                            <textarea type="text" className="input-editing" rows="5" placeholder="Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."/>
+                            <label><h5>Description</h5></label>
+                            <textarea type="text" for="Description" className="input-editing" rows="5" placeholder="Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."/>
                         </div>
                     </div>
                 </div>
