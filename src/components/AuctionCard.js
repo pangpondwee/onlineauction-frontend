66973262e4edd3bc03ad1cada0595af405cacba3
kong-wep <pakkapol.20100@gmail.com>
2022-09-21 14:07:09 +0700

    Home get data from api

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -3,11 +3,11 @@ import {Link} from "react-router-dom";
 const AuctionCard = (props) =>{
 	return (		
 		<div className="card border-light mb-3">
-			<div className="">
+			<div className="cardBody">
 				<Link to="#" className="img-link">
 					<img src={props.picture} className="card-img-top"></img>
 				</Link>
-				<div className="timeBox">{props.time} left</div>
+				<div className="timeBox">{props.time}</div>
 				<Link to="#" className="itemName-link">
 					<p className="itemName">{props.name}</p>
 				</Link>
