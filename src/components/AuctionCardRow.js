bfb26064bcae85aa66c11640edc88996aeae6fc9
kong-wep <pakkapol.20100@gmail.com>
2022-09-21 14:07:09 +0700

    Home get data from api

--- src/components/AuctionCardRow.js
+++ src/components/AuctionCardRow.js
@@ -2,135 +2,43 @@ import {Link} from "react-router-dom";
 import AuctionCard from "../components/AuctionCard";
 import arrow_left from "../pictures/arrow_left.png";
 import arrow_right from "../pictures/arrow_right.png";
-import AuctionCardForRow from "./AuctionCardForRow";
+// import AuctionCardForRow from "./AuctionCardForRow";
+function getDate(timeRemaining){
+	// TODO make date lighter
+	const d = new Date(Number(timeRemaining));
+	const d_days = Math.floor(timeRemaining/(24*60*60*1000)); // days remaining
+	const d_hour = d.getHours();
+	const d_minute = d.getMinutes();
+	const d_seconds = d.getSeconds();
+	if(timeRemaining <= 0){
+		return "Ended";
+	}
+	if(d_days > 2){
+		return `${d_days} day(s)`;
+	}
+	else{
+		return `${d_hour}hr ${d_minute}m ${d_seconds}s`;
+	}
+}
 
 const AuctionCardRow = (props) =>{
-    console.log(props);
     let auctionCard_element = []
-    for(let i=0;i<15;i++){
+    for(let i=0;i<props.data.length;i++){
+        const timeRemaining = Number(props.data[i].endDate) - Date.now()
+        const price = props.data[i].currentPrice ? props.data[i].currentPrice : "Unknown";
         auctionCard_element.push(
-            <AuctionCardForRow key={i} name={props.data[i].name} price={props.data[i].price} picture={props.data[i].picture}/>
+            <AuctionCard 
+            key={i} 
+            name={props.data[i].productName} 
+            price={price} 
+            picture={"#"}
+            time={getDate(timeRemaining)}/>
         )
     }
 	return (
-        <>
         <div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
             {auctionCard_element}
         </div>
-        	
-		{/* <div className="card-deck"> */}
-        {/* <div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
-            <AuctionCard name={props.name1} price={props.price1} time={props.time1} picture={props.picture1}/>
-            <AuctionCard name={props.name2} price={props.price2} time={props.time2} picture={props.picture2}/>
-            <AuctionCard name={props.name3} price={props.price3} time={props.time3} picture={props.picture3}/>
-            <AuctionCard name={props.name4} price={props.price4} time={props.time4} picture={props.picture4}/>
-            <AuctionCard name={props.name5} price={props.price5} time={props.time5} picture={props.picture5}/>
-            <AuctionCard name={props.name6} price={props.price6} time={props.time6} picture={props.picture6}/>
-            <AuctionCard name={props.name7} price={props.price7} time={props.time7} picture={props.picture7}/>
-            <AuctionCard name={props.name8} price={props.price8} time={props.time8} picture={props.picture8}/>
-            <AuctionCard name={props.name9} price={props.price9} time={props.time9} picture={props.picture9}/>
-            <AuctionCard name={props.name10} price={props.price10} time={props.time10} picture={props.picture10}/>
-            <AuctionCard name={props.name11} price={props.price11} time={props.time11} picture={props.picture11}/>
-            <AuctionCard name={props.name12} price={props.price12} time={props.time12} picture={props.picture12}/>
-            <AuctionCard name={props.name13} price={props.price13} time={props.time13} picture={props.picture13}/>
-            <AuctionCard name={props.name14} price={props.price14} time={props.time14} picture={props.picture14}/>
-            <AuctionCard name={props.name15} price={props.price15} time={props.time15} picture={props.picture15}/>
-		</div> */}
-        {/* <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <h1>First page</h1>
-                    <br></br>        
-                    <div className="d-block">            
-                    <AuctionCard name={props.name1} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name2} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name3} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name4} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name5} price={props.price} time={props.time}/>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <h1>Second</h1>
-                    <br></br>
-                    <AuctionCard name={props.name6} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name7} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name8} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name9} price={props.price} time={props.time}/>
-                    <AuctionCard name={props.name10} price={props.price} time={props.time}/>
-                </div>
-                <div className="carousel-item ">
-                    <h1>Third page</h1>
-                    <br></br>
-                    <div className="d-block">
-                        <AuctionCard name={props.name11} price={props.price} time={props.time}/>
-                        <AuctionCard name={props.name12} price={props.price} time={props.time}/>
-                        <AuctionCard name={props.name13} price={props.price} time={props.time}/>
-                        <AuctionCard name={props.name14} price={props.price} time={props.time}/>
-                        <AuctionCard name={props.name15} price={props.price} time={props.time}/>
-                    </div>
-                </div>
-            </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
-        </div> */}
-        {/* <section id="slider">
-            <div className="container">
-                <div className="subcontainer">
-                    <div className="slider-wrapper">
-                        <div className="controller">
-                            <div id="controls">
-                                <button className="previous"><img src={arrow_left} className="button"></img></button>
-                                <button className="next"><img src={arrow_right} className="button"></img></button>
-                            </div>
-                            <br></br>
-                            <div className="my-slider">
-                                <div>
-                                    <div className="slide">
-                                        <div className="slide-card">
-                                            <AuctionCard name={props.name1} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name2} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name3} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name4} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name5} price={props.price} time={props.time}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="slide">
-                                        <div className="slide-card">
-                                            <AuctionCard name={props.name1} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name2} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name3} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name4} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name5} price={props.price} time={props.time}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="slide">
-                                        <div className="slide-card">
-                                            <AuctionCard name={props.name1} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name2} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name3} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name4} price={props.price} time={props.time}/>
-                                            <AuctionCard name={props.name5} price={props.price} time={props.time}/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> */}
-        
-        </>
 	)
 }
 export default AuctionCardRow;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,82 +5,51 @@ import "../css/Home.css";
 import arrow_left from "../pictures/arrow_left.png";
 import arrow_right from "../pictures/arrow_right.png";
 import AuctionCardRow from "../components/AuctionCardRow";
+import getData from "../components/fetchData";
+
 const Home = () =>{
-	const auctionData = [
-		{name:"first", price:"2000", picture:"https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c21hbGx8ZW58MHx8MHx8&w=1000&q=80"},
-		{name:"second", price:"2000", picture:"https://www.skipprichard.com/wp-content/uploads/2015/12/bigstock-D-Small-People-Newtons-Crad-65362909.jpg"},
-		{name:"third", price:"2000", picture:"https://images.pexels.com/photos/1459495/pexels-photo-1459495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
-		{name:"fourth", price:"2000", picture:arrow_left},
-		{name:"fifth", price:"2000", picture:arrow_right},
-		{name:"six", price:"2000", picture:arrow_left},
-		{name:"seven", price:"2000", picture:arrow_right},
-		{name:"eight", price:"2000", picture:arrow_left},
-		{name:"nine", price:"2000", picture:arrow_right},
-		{name:"ten", price:"2000", picture:arrow_left},
-		{name:"eleven", price:"2000", picture:arrow_right},
-		{name:"twelve", price:"2000", picture:arrow_left},
-		{name:"thirteen", price:"2000", picture:arrow_right},
-		{name:"fourteen", price:"2000", picture:arrow_left},
-		{name:"fifteen", price:"2000", picture:arrow_right},
-	]
-	// useEffect(()=>{
-
-	// },[]);
-
+	const [popular,setPopular] = useState([]);
+	const [recent,setRecent] = useState([]);
+	const [ending,setEnding] = useState([]);
+	const [following,setFollowing] = useState([]);
+	const displayName = localStorage.getItem("displayName");
+	useEffect(()=>{
+		// Popular
+		getData(
+		'http://13.250.98.9/api/auction/auction-list?filter=popular',
+		).then((res) => {
+			setPopular(res.data)
+		})
+		// Recent bidding
+		getData(
+		'http://13.250.98.9/api/auction/auction-list?filter=recent_bidding',
+		).then((res) => {
+			setRecent(res.data)
+		})
+		// Ending Soon
+		getData(
+		'http://13.250.98.9/api/auction/auction-list?filter=ending_soon',
+		).then((res) => {
+			setEnding(res.data)
+		})
+		// My following list
+		getData(
+		'http://13.250.98.9/api/auction/auction-list?filter=my_following_list',
+		).then((res) => {
+			setFollowing(res.data)
+		})
+	},[])
 	return (
 		<>
 		<p className="headHome">Welcome, Peeranat! Let’s see what you got...</p>		
 		<p className="detail">Your Recent Bids</p>
-		<div className="flexRow">
-			{/* previous button */}
-			<Link to={"#"}>
-				<img src={arrow_left} className="button"></img>
-			</Link>
-			<AuctionCard name="first" price="2000$" time="18:30:22" picture="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c21hbGx8ZW58MHx8MHx8&w=1000&q=80"/>
-			<AuctionCard name="second" price="100$" time="time" picture="https://www.skipprichard.com/wp-content/uploads/2015/12/bigstock-D-Small-People-Newtons-Crad-65362909.jpg"/>
-			<AuctionCard name="third" price="1000$" time="time" picture="https://images.pexels.com/photos/1459495/pexels-photo-1459495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
-			<AuctionCard name="Omnitrix" price="20$" time="08:53:05"picture="https://cdn.estore.nu/68744-thickbox_default/ben-10---deluxe-omnitrix-creator-set.jpg"/>
-			<AuctionCard name="e"/>
-			{/* next button */}
-			<Link to={"#"}>
-				<img src={arrow_right} className="button"></img>
-			</Link>
-		</div>
+		{recent.length > 0 ? <AuctionCardRow data={recent}/> : <p>You have no recent bids</p> }
 		<p className="detail">Recent Following List</p>
-
-		<AuctionCardRow data={auctionData}/>
-
-		<div className="flexRow">
-			{/* Add previous button */}
-			<Link to={"#"}>
-				<img src={arrow_left} className="button"></img>
-			</Link>
-			<AuctionCard name="first"/>
-			<AuctionCard name="second"/>
-			<AuctionCard name="c"/>
-			<AuctionCard name="d"/>
-			<AuctionCard name="e"/>
-			{/* Add next button */}
-			<Link to={"#"}>
-				<img src={arrow_right} className="button"></img>
-			</Link>
-		</div>
+		{following.length > 0 ? <AuctionCardRow data={following}/> : <p>You are not following any bids</p> }
 		<p className="detail">Popular</p>
-		<div className="flexRow">
-			<AuctionCard name="first"/>
-			<AuctionCard name="second"/>
-			<AuctionCard name="c"/>
-			<AuctionCard name="d"/>
-			<AuctionCard name="e"/>
-		</div>
+		{popular.length > 0 ? <AuctionCardRow data={popular}/> : <p>There are no popular bids</p> }
 		<p className="detail">Ending soon</p>
-		<div className="flexRow">
-			<AuctionCard name="first"/>
-			<AuctionCard name="second"/>
-			<AuctionCard name="c"/>
-			<AuctionCard name="d"/>
-			<AuctionCard name="e"/>
-		</div>
+		{ending.length > 0 ? <AuctionCardRow data={ending}/> : <p>There are no bids ending soon</p> }
 		</>
 	)
 }
