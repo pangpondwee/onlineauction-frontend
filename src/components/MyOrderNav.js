b42b7da30c692dbfa0216570794d0ba5badb0f36
ZisZtR <zisztr@gmail.com>
2022-09-24 02:13:02 +0700

    prepare to get data, change format for order page

--- src/components/MyOrderNav.js
+++ src/components/MyOrderNav.js
@@ -1,50 +1,63 @@
 import {Link} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const MyAuctionNav = () =>{
+	const location = useLocation()
+	
+	const c_tab = (search) => (
+		search===location.search.split("?")[2]? "current-tab" : ""
+    )
+
 	return (
 		<nav className="My-Order-Nav">
-			<div className="Order-Menu current-tab">
-                <Link to="/account/myauction/order" className="menu-link">All</Link>
+			<div className={`Order-Menu ${c_tab("all")}`}>
+                <Link to="/account/myorder?auction?all" className="menu-link">All</Link>
 			</div>
-			<div className="Order-Menu">
-                <Link to="" className="menu-link">Currently On Auction</Link>
+			<div className={`Order-Menu ${c_tab("current")}`}>
+                <Link to="/account/myorder?auction?current" className="menu-link">Currently On Auction</Link>
 			</div>
-			<div className="Order-Menu">
-                <Link to="" className="menu-link">To Pay</Link>
+			<div className={`Order-Menu ${c_tab("pay")}`}>
+                <Link to="/account/myorder?auction?pay" className="menu-link">To Pay</Link>
 			</div>
-			<div className="Order-Menu">
-                <Link to="" className="menu-link">To Shipped</Link>
+			<div className={`Order-Menu ${c_tab("shipped")}`}>
+                <Link to="/account/myorder?auction?shipped" className="menu-link">To Shipped</Link>
 			</div>
-			<div className="Order-Menu">
-                <Link to="" className="menu-link">To Confirm</Link>
+			<div className={`Order-Menu ${c_tab("confirm")}`}>
+                <Link to="/account/myorder?auction?confirm" className="menu-link">To Confirm</Link>
 			</div>
-			<div className="Order-Menu">
-                <Link to="" className="menu-link">Completed</Link>
+			<div className={`Order-Menu ${c_tab("complete")}`}>
+                <Link to="/account/myorder?auction?complete" className="menu-link">Completed</Link>
 			</div>
 		</nav>
 	)
 }
 
 export const MyBidNav = () =>{
+	const location = useLocation()
+	
+	const c_tab = (search) => (
+		search===location.search.split("?")[2]? "current-tab" : ""
+    )
+
 	return (
 		<nav className="My-Order-Nav">
-			<div className="Order-Menu current-tab">
-				<Link to="/account/myorder" className="menu-link">All</Link>
+			<div className={`Order-Menu ${c_tab("all")}`}>
+				<Link to="/account/myorder?bid?all" className="menu-link">All</Link>
 			</div>
-			<div className="Order-Menu">
-				<Link to="" className="menu-link">Currently Bidding</Link>
+			<div className={`Order-Menu ${c_tab("current")}`}>
+				<Link to="/account/myorder?bid?current" className="menu-link">Currently Bidding</Link>
 			</div>
-			<div className="Order-Menu">
-				<Link to="" className="menu-link">To Pay</Link>
+			<div className={`Order-Menu ${c_tab("pay")}`}>
+				<Link to="/account/myorder?bid?pay" className="menu-link">To Pay</Link>
 			</div>
-			<div className="Order-Menu">
-				<Link to="" className="menu-link">To Delivered</Link>
+			<div className={`Order-Menu ${c_tab("delivered")}`}>
+				<Link to="/account/myorder?bid?delivered" className="menu-link">To Delivered</Link>
 			</div>
-			<div className="Order-Menu">
-				<Link to="" className="menu-link">To Confirm</Link>
+			<div className={`Order-Menu ${c_tab("confirm")}`}>
+				<Link to="/account/myorder?bid?confirm" className="menu-link">To Confirm</Link>
 			</div>
-			<div className="Order-Menu">
-				<Link to="" className="menu-link">Completed</Link>
+			<div className={`Order-Menu ${c_tab("complete")}`}>
+				<Link to="/account/myorder?bid?complete" className="menu-link">Completed</Link>
 			</div>
 		</nav>
 	)
