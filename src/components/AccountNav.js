57083f97d021670f27143ffb5d82c87b1976b255
ZisZtR <zisztr@gmail.com>
2022-09-24 02:13:02 +0700

    prepare to get data, change format for order page

--- src/components/AccountNav.js
+++ src/components/AccountNav.js
@@ -2,30 +2,36 @@ import {Link, useLocation} from "react-router-dom";
 
 const AccountNav = () =>{
 	const location = useLocation();
-	const c_tab = (tag) => ((tag===location.pathname.slice(9))? "current-tab" : "")
- 
+	const c_tab = (tag, search) => {
+		if(tag === "myorder"){
+			return search===location.search.split("?")[1]? "current-tab" : "";
+		}
+		else {
+			return tag===location.pathname.split("/")[2]? "current-tab" : "";
+		}
+	}
 
 	return (
 		<div className="lefthand-nav">
 			{/* class "current-tab" 'll highlight depends on current page */}
-			<div className={`menu ${c_tab("profile")}`}>
+			<div className={`menu ${c_tab("profile", " ")}`}>
 				<Link to="/account/profile" className="menu-link">My Profile</Link>
 			</div>
 			<div className="menu">
 				<h5>My Order</h5>
 				<ul className="no-bullets">
-					<li className={`menu-dropdown ${c_tab("myorder")}`}>
-						<Link to="/account/myorder?bid" className="menu-link">My Bid</Link>
+					<li className={`menu-dropdown ${c_tab("myorder", "bid")}`}>
+						<Link to="/account/myorder?bid?all" className="menu-link">My Bid</Link>
 					</li>
-					<li className={`menu-dropdown ${c_tab("myorder")}`}>
-						<Link to="/account/myorder?auction" className="menu-link">My Auction</Link>
+					<li className={`menu-dropdown ${c_tab("myorder", "auction")}`}>
+						<Link to="/account/myorder?auction?all" className="menu-link">My Auction</Link>
 					</li>
 				</ul>
 			</div>
-			<div className={`menu ${c_tab("review")}`}>
+			<div className={`menu ${c_tab("review", "")}`}>
 				<Link to="/account/review" className="menu-link">My Review</Link>
 			</div>
-			<div className={`menu ${c_tab("following")}`}>
+			<div className={`menu ${c_tab("following", "")}`}>
 				<Link to="/account/following" className="menu-link">My Following List</Link>
 			</div>
 		</div>
--- src/pages/MyOrder.js
+++ src/pages/MyOrder.js
@@ -1,26 +1,55 @@
 import OrderObj from "../components/OrderObj";
 import {MyBidNav, MyAuctionNav} from "../components/MyOrderNav";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function text_alert(status){
+    if(status==="currently_bid") return "Your last bid: 1800$"
+    else if(status==="bidder-to-pay") return "Waiting for your payment"
+    else if(status==="bidder-to-pay-wait-admin") return "Waiting for admin to confirm your payment"
+    else if(status==="to-delivered") return "Waiting for auctioneer to ship your item"
+    else if(status==="To Confirm") return "An item is on its way to you. You can check a tracking number here.If you’re satisfy with your item, click accept."
+    else if(status==="on-auction") return "Time left: 13 hr 13 m 13 s"
+    else if(status==="auctioneer-to-pay") return "Waiting for payment from Pongsatorn (Bidder)"
+    else if(status==="to-shipped") return "Waiting for your payment"
+    else if(status==="auctioneer-to-confirm") return "Waiting for bidder to confirm"
+    else return
+}
 
 const MyOrder = () =>{
+
     const location = useLocation()
+
+    const [data,setData] = useState({});
+    const [status,setStatus]=useState("unknown");
+
+    // useEffect(()=>{
+	// 	getData(`/api/user/myorder?list='`).then((res)=>{
+	// 		setStatus(res.status);
+	// 		if(res.status == "success"){
+	// 			setData(res.data);
+	// 		}
+	// 		else{
+	// 			setData(res.message);
+	// 		}
+	// 	})
+	// },[]);
 
 	return (
         <>
-            {(location.search === "?bid")? <MyBidNav/> : <MyAuctionNav/>}
+            {(location.search.split("?")[1] === "bid")? <MyBidNav/> : <MyAuctionNav/>}
             <div className="all-review">
                 {/* store each order */}
-                <OrderObj status="Currently Bid" status_class="currently_bid" text_alert="Your last bid: 1800$" by_who="(By Kong Pakkapol)"/>
-                <OrderObj status="To Pay" status_class="bidder-to-pay" text_alert="Waiting for your payment" by_who="(By Kong Pakkapol)"/>
-                <OrderObj status="To Pay" status_class="bidder-to-pay-wait-admin" text_alert="Waiting for admin to confirm your payment" by_who="(By Kong Pakkapol)"/>
-                <OrderObj status="To Delivered" status_class="to-delivered" text_alert="Waiting for auctioneer to ship your item" by_who="(By Kong Pakkapol)"/>
-                <OrderObj status="To Confirm" status_class="bidder-to-confirm" text_alert="An item is on its way to you. You can check a tracking number here.
-If you’re satisfy with your item, click accept." by_who="(By Kong Pakkapol)" />
+                <OrderObj status="Currently Bid" status_class="currently_bid" text_alert={text_alert("currently_bid")} by_who="(By Kong Pakkapol)"/>
+                <OrderObj status="To Pay" status_class="bidder-to-pay" text_alert={text_alert("bidder-to-pay")} by_who="(By Kong Pakkapol)"/>
+                <OrderObj status="To Pay" status_class="bidder-to-pay-wait-admin" text_alert={text_alert("bidder-to-pay-wait-admin")} by_who="(By Kong Pakkapol)"/>
+                <OrderObj status="To Delivered" status_class="to-delivered" text_alert={text_alert("to-delivered")} by_who="(By Kong Pakkapol)"/>
+                <OrderObj status="To Confirm" status_class="bidder-to-confirm" text_alert={text_alert("bidder-to-confirm")} by_who="(By Kong Pakkapol)" />
                 <OrderObj status="Completed" status_class="completed" by_who="(By Kong Pakkapol)"/>
-                <OrderObj status="On Auction" status_class="on-auction" text_alert="Time left: 13 hr 13 m 13 s"/>
-                <OrderObj status="To Pay" status_class="auctioneer-to-pay" text_alert="Waiting for payment from Pongsatorn (Bidder)"/>
-                <OrderObj status="To Shipped" status_class="to-shipped" text_alert="Waiting for shipping"/>
-                <OrderObj status="To Confirm" status_class="auctioneer-to-confirm" text_alert="Waiting for bidder to confirm"/>
+                <OrderObj status="On Auction" status_class="on-auction" text_alert={text_alert("on-auction")}/>
+                <OrderObj status="To Pay" status_class="auctioneer-to-pay" text_alert={text_alert("auctioneer-to-pay")}/>
+                <OrderObj status="To Shipped" status_class="to-shipped" text_alert={text_alert("currently_bid")}/>
+                <OrderObj status="To Confirm" status_class="auctioneer-to-confirm" text_alert={text_alert("to-shipped")}/>
                 <OrderObj status="Completed" status_class="completed"/>
             </div>
         </>
