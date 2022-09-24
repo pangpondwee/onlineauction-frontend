d1e1c9c3c813c8286f0c7b5aaabee18cd5000668
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-24 23:08:28 +0700

    Add promptpayqr gen and add popupconfirmsubmit

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -5,7 +5,9 @@ import { FilePond, registerPlugin } from 'react-filepond'
 import { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { postData, getData } from '../components/fetchData'
-import Button from 'react-bootstrap/Button'
+import { generatePayload } from '../components/promptpay'
+import { QRCodeSVG } from 'qrcode.react'
+import PopupConfirmSubmit from '../components/PopupConfirmSubmit'
 
 // Import FilePond styles
 import 'filepond/dist/filepond.min.css'
@@ -22,6 +24,11 @@ registerPlugin(
   FilePondPluginFileValidateType
 )
 
+const PromptpayQR = (props) => {
+  let payload = generatePayload(props.ppID, props.amount)
+  return <QRCodeSVG value={payload} />
+}
+
 const Payment = () => {
   // const { auctionId } = useParams()
   const auctionId = '632c09fb1e43a833d78ad748'
@@ -36,10 +43,7 @@ const Payment = () => {
   const [transactionDateTime, setTransactionDateTime] = useState()
   const [value, setValue] = useState()
   const uploadFileRef = useRef()
-  const [show, setShow] = useState(false)
-
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const [modalShow, setModalShow] = useState(false)
 
   useEffect(() => {
     getData(`/payment/${auctionId}`)
@@ -48,7 +52,7 @@ const Payment = () => {
         setItemName(res.data.productName)
         setAuctioneerName(res.data.auctioneerName)
         setPrice(res.data.winningPrice)
-        setProductPicture('base64:' + res.data.productPicture)
+        setProductPicture(res.data.productPicture)
       })
       .catch((e) => console.log(e))
   }, [])
@@ -69,8 +73,7 @@ const Payment = () => {
       })
       .catch((e) => console.log(e))
   }
-  const submitHandler = (event) => {
-    event.preventDefault()
+  const submitHandler = () => {
     const uploadedFile = uploadFileRef.current.getFiles()
     let billingInfo = {
       bidderNamre: fullName,
@@ -86,7 +89,13 @@ const Payment = () => {
     <div className="page-with-summary">
       <div className="form-section">
         <h1 className="header">Payment</h1>
-        <form className="payment-form">
+        <form
+          className="payment-form"
+          onSubmit={(event) => {
+            setModalShow(true)
+            event.preventDefault()
+          }}
+        >
           <div className="form-heading1">BILLING INFO</div>
           <div className="sub-form">
             <div className="form-input-field">
@@ -99,6 +108,7 @@ const Payment = () => {
                 value={fullName}
                 onChange={(event) => setFullName(event.target.value)}
                 placeholder="e.g. Peeranat Srisuthangkul"
+                required
               ></input>
             </div>
             <div className="form-input-field">
@@ -143,11 +153,7 @@ const Payment = () => {
           <div className="form-heading1">TRANSACTION INFO</div>
           <div className="sub-form">
             <div className="center-pic form-input-field">
-              <img
-                className="promptpayqrpic"
-                src={promptpayqr}
-                alt="promptpayqr"
-              ></img>
+              <PromptpayQR ppID={'0909754062'} amount={2000} />
             </div>
             <div className="form-input-field">
               <label htmlFor="uploadTransactionSlip" className="form-label">
@@ -185,9 +191,9 @@ const Payment = () => {
               ></input>
             </div>
             <div>
-              <Button variant="primary" onClick={() => handleShow}>
-                Launch vertically centered modal
-              </Button>
+              <button type="submit" className="btn btn-primary first-button">
+                Submit
+              </button>
               <button type="button" className="btn btn-outline-primary">
                 Cancel
               </button>
@@ -203,6 +209,11 @@ const Payment = () => {
           productPicture={productPicture}
         ></PaymentSummaryCard>
       </div>
+      <PopupConfirmSubmit
+        modalShow={modalShow}
+        submitHandler={submitHandler}
+        setModalShow={setModalShow}
+      />
     </div>
   )
 }
