ec9b57e267da3b479a9be48a75d496508e15c1ed
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-09-24 23:52:22 +0700

    Edit payment path

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Route path="forget-password" element={<ForgetPassword />} />
           <Route path="reset-password" element={<ResetPassword />} />
           <Route path="place-auction" element={<PlaceAuction />} />
-          <Route path="payment" element={<Payment />} />
+          <Route path="payment/:auctionId" element={<Payment />} />
           <Route path="shipping" element={<Shipping />} />
           <Route path="billing-info" element={<BillingInfo />} />
           <Route path="auctioneer/:auctioneerID" element={<ViewAuctioneer />} />
