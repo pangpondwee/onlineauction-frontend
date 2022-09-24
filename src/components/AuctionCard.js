695f13143e9cdd8a81f38beafeff6e7a0c98c91d
kong-wep <pakkapol.20100@gmail.com>
2022-09-24 10:06:48 +0700

    Added red ending soon timebox

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -19,13 +19,17 @@ function getDate(timeRemaining){
 }
 
 const AuctionCard = (props) =>{
+	let timeClass = "timeBox"
+	if(props.time < 24*60*60*1000){
+		timeClass=timeClass+" timeBox-red" // red if time less than 1 day
+	}
 	return (		
 		<div className="card border-light mb-3">
 			<div className="cardBody">
 				<Link to={"/auction/"+props.id} className="img-link">
 					<img src={props.picture} className="card-img-top"></img>
 				</Link>
-				<div className="timeBox">{getDate(props.time)}</div>
+				<div className={timeClass}>{getDate(props.time)}</div>
 				<Link to="#" className="itemName-link">
 					<p className="itemName">{props.name}</p>
 				</Link>
