aac3ab8c161ad2c0e32a1cc5f4eb6b961c51aa8f
kong-wep <pakkapol.20100@gmail.com>
2022-09-26 00:37:08 +0700

    Verify email complete

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ function App() {
 			    <Route path="auction/:auctionId" element={<Auction />} />
           <Route path="forget-password" element={<ForgetPassword />} />
           <Route path="reset-password" element={<ResetPassword />} />
-          <Route path="verify-email" element={<VerifyEmail />} />
           <Route path="place-auction" element={<PlaceAuction />} />
           <Route path="payment" element={<Payment />} />
           <Route path="shipping" element={<Shipping />} />
@@ -44,6 +43,7 @@ function App() {
           <Route path="faq" element={<FAQ />} />
 			    <Route path="search" element={<Search />} />
 			    <Route path="search/:pageNumber" element={<Search />} />
+          <Route path="account/verify/:userId" element={<VerifyEmail />} />
           <Route path="account" element={<AccountLayout/>}>
             <Route path="profile" element={<AccountPage/>}/>
             <Route path="edit" element={<AccountEdit/>}/>
