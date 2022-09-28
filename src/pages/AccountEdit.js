e5021d6a993dc60b4e3a0aafc48a3b2df6cac7af
ZisZtR <zisztr@gmail.com>
2022-09-28 22:27:57 +0700

    get&patch in profile checked

--- src/pages/AccountEdit.js
+++ src/pages/AccountEdit.js
@@ -1,6 +1,6 @@
 import blank_profile from "../pictures/blank_profile.png";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {getData, patchData} from '../components/fetchData';
 
 const AccountEdit = (props) =>{
@@ -10,29 +10,31 @@ const AccountEdit = (props) =>{
 
     const previewImage = (event)=>{ setPrepic(URL.createObjectURL(event.target.files[0])) }
 
-    const [data,setData] = useState({
-        displayName: "Peeranut Srisuthangkul",
-        email: "peeranut.sri@ku.th",
-        phoneNumber: "06200000000",
-        address: "4412 Matlock Rd #200 Arlington Texas United States 76018",
-        description: "Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."});
+    // const [data,setData] = useState({
+    //     displayName: "Peeranut Srisuthangkul",
+    //     email: "peeranut.sri@ku.th",
+    //     phoneNumber: "06200000000",
+    //     address: "4412 Matlock Rd #200 Arlington Texas United States 76018",
+    //     description: "Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."});
+    const [data, setData] = useState({})
     const [status,setStatus]=useState("unknown");
 
     const sendChange = (new_data) =>{
-        // patchData(`'/api/user/edit'`,JSON.stringify(new_data))
-        // .then((res)=>{
-        //     if(!res.status) throw new Error("Could not get status")
-        //     if(res.status == "fail" || res.status == "error" || res.status == "err") throw new Error(res.message)
-        //     navigate(`/account/profile`)
-        // })
-        // .catch(e=>{
-        //     console.log(e.message)
-        // })
+        patchData(`/user/edit`,JSON.stringify(new_data))
+        .then((res)=>{
+            if(!res.status) throw new Error("Could not get status")
+            if(res.status == "fail" || res.status == "error" || res.status == "err") throw new Error(res.message)
+            console.log(res.status)
+            //navigate(`/account/profile`)
+        })
+        .catch(e=>{
+            console.log(e.message)
+        })
         console.log(new_data)
     }
 
     const summitChange = (e) =>{
-        const personal_info = ["Name", "Email", "PhoneNumber", "Address", "Description"]
+        const personal_info = ["displayName", "email", "phoneNumber", "address", "accountDescription"]
         const _changed = {}
         personal_info.forEach((_info)=>{
             const tmp = document.getElementById(_info).value
@@ -45,17 +47,19 @@ const AccountEdit = (props) =>{
 		e.preventDefault()
     }
 
-    // useEffect(()=>{
-	// 	getData(`/api/user/myprofile`).then((res)=>{ 
-	// 		setStatus(res.status);
-	// 		if(res.status == "success"){
-	// 			setData(res.data);
-	// 		}
-	// 		else{
-	// 			setData(res.message);
-	// 		}
-	// 	})
-	// },[]);
+    useEffect(()=>{
+		getData(`/user/myprofile`).then((res)=>{ 
+			setStatus(res.status);
+			if(res.status == "success"){
+				setData(res.data);
+			}
+			else{
+				setData(res.message);
+			}
+		})
+	},[]);
+
+    // console.log(data)
 
     const _changed = []
 
@@ -65,30 +69,30 @@ const AccountEdit = (props) =>{
                 <h1>My Profile</h1>
                 <div className="main-editing-page">
                     <div>
-                        <img className="profile-pic" alt="ProfilePic" id="Profile" src={prepic}/>
+                        <img className="profile-pic" alt="ProfilePic" id="Profile" src={data.profilePicture}/>
                         <label for="input_pic" className="choose-image">Choose Image</label>
                         <input id="input_pic" type="file" className="choose-image" name="yourPic" accept="image/*" onChange={previewImage} hidden/>
                     </div>
                     <div>
                         <div>
                             <label><h5>Name</h5></label><br/>
-                            <input type="text" id="Name" className="input-editing" placeholder={data.displayName}/>
+                            <input type="text" id="displayName" className="input-editing" placeholder={data.displayName}/>
                         </div>
                         <div>
                             <label><h5>Email</h5></label><br/>
-                            <input type="email" id="Email" className="input-editing" placeholder={data.email}/>
+                            <input type="email" id="email" className="input-editing" placeholder={data.email}/>
                         </div>
                         <div>
                             <label><h5>Phone Number</h5></label><br/>
-                            <input type="number" id="PhoneNumber" className="input-editing" placeholder={data.phoneNumber}/>
+                            <input type="number" id="phoneNumber" className="input-editing" placeholder={data.phoneNumber}/>
                         </div>
                         <div>
                             <label><h5>Address</h5></label><br/>
-                            <textarea type="text" id="Address" className="input-editing" placeholder={data.address}/>
+                            <textarea type="text" id="address" className="input-editing" placeholder={data.address}/>
                         </div>
                         <div>
                             <label><h5>Description</h5></label><br/>
-                            <textarea type="text" id="Description" className="input-editing" rows="7" placeholder={data.description}/>
+                            <textarea type="text" id="accountDescription" className="input-editing" rows="7" placeholder={data.description}/>
                         </div>
                     </div>
                 </div>
--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -12,40 +12,47 @@ import {getData} from '../components/fetchData';
 import "../css/AccountPage.css";
 
 const AccountPage = () =>{
-    const userId = useParams()
-    const [data,setData] = useState({
-        displayName: "Peeranut Srisuthangkul",
-        email: "peeranut.sri@ku.th",
-        phoneNumber: "06200000000",
-        address: "4412 Matlock Rd #200 Arlington Texas United States 76018",
-        description: "Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."});
+    
+    // const [data,setData] = useState({
+    //     displayName: "Peeranut Srisuthangkul",
+    //     email: "peeranut.sri@ku.th",
+    //     phoneNumber: "06200000000",
+    //     address: "4412 Matlock Rd #200 Arlington Texas United States 76018",
+    //     description: "Exercitation sint fugiat et esse ut do quis laboris anim nisi proident ullamco. Sit eu mollit cillum et consequat eu consectetur ad reprehenderit exercitation sunt duis nisi est. Pariatur exercitation commodo non tempor. Fugiat minim velit veniam reprehenderit nulla veniam voluptate adipisicing ullamco culpa incididunt. Sunt irure commodo et ut do aute duis."});
+    const [data,setData] = useState({})
     const [status,setStatus]=useState("unknown");
     const [other_data, setOther_data] = useState({"badge": [badge1, badge2, badge3],
                                                 "rating": 3.5,
                                                 "totalAuctioned" : 12,
                                                 "successAuctioed" : 5})
 
-    // useEffect(()=>{
-	// 	getData(`/api/user/myprofile`).then((res)=>{ 
-	// 		setStatus(res.status);
-	// 		if(res.status == "success"){
-	// 			setData(res.data);
-	// 		}
-	// 		else{
-	// 			setData(res.message);
-	// 		}
-	// 	})
+    useEffect(()=>{
+		getData(`/user/myprofile`).then((res)=>{ 
+			setStatus(res.status);
+			if(res.status == "success"){
+				setData(res.data);
+			}
+			else{
+				setData(res.message);
+			}
+		})
+	},[]);
 
-    //     getData(`/api/user/profile/${userId}).then((res)=>{ 
-	// 		setStatus(res.status);
-	// 		if(res.status == "success"){
-	// 			setOther_data(res.data);
-	// 		}
-	// 		else{
-	// 			setOther_data(res.message);
-	// 		}
-	// 	})
-	// },[]);
+    let userId = data._id;
+    useEffect(()=>{
+        getData(`/user/profile/${userId}`).then((res)=>{ 
+			setStatus(res.status);
+			if(res.status == "success"){
+				setOther_data(res.data);
+			}
+			else{
+				setOther_data(res.message);
+			}
+		})
+    }, [userId])
+
+    console.log(data)
+    console.log(other_data)
 
     const allstar = []
 
@@ -67,7 +74,7 @@ const AccountPage = () =>{
             </div>
             <div className="main-info-profile">
                 {/* need to add pic */}
-                <img className="profile-pic" alt="ProfilePic" src={blank_profile}/>
+                <img className="profile-pic" alt="ProfilePic" src={data.profilePicture}/>
                 <div>
                     <h5>Display Name</h5>
                     <div className="account-info"><h5>{data.displayName}</h5></div>
@@ -83,22 +90,22 @@ const AccountPage = () =>{
                 <div className="about-you">
                     <h5>Description</h5>
                     <h6>
-                        {data.description}
+                        {data.accountDescription}
                     </h6>
                 </div>
                 <div className="about-you">
                     <h5>My Badges & Statistics</h5>
                     <div>
-                        <img src={other_data.badge[0]} className="badge-pic" alt="BadgePic"/>
+                        {/* <img src={other_data.badge[0]} className="badge-pic" alt="BadgePic"/>
                         <img src={other_data.badge[1]} className="badge-pic" alt="BadgePic"/>
-                        <img src={other_data.badge[2]} className="badge-pic" alt="BadgePic"/>
+                        <img src={other_data.badge[2]} className="badge-pic" alt="BadgePic"/> */}
                     </div>
                     <div className="d-flex">
                         <h6>Average Rating:</h6>
                         {allstar}
                     </div>
                     <h6>Items submitted: {other_data.totalAuctioned}</h6>
-                    <h6>Items sold: {other_data.successAuctioed}</h6>
+                    <h6>Items sold: {other_data.successAuctioned}</h6>
                 </div>
             </div>
             
