6fbaf11d738d2cf6bde2e7f7e32fd43c9e0fca62
kong-wep <pakkapol.20100@gmail.com>
2022-10-05 22:52:25 +0700

    For loop to map

--- src/pages/Auction.js
+++ src/pages/Auction.js
@@ -19,22 +19,21 @@ function getHistory(data){
 
 const HistoryModal = (props)=>{
 	const history=props.history
-	let history_elements = []
-	for(let i=0;i<history.length;i++){
-		const ms = Number(history[i].biddingDate)
-		const d = new Date(ms)
+	let history_elements = props.history.map((item,index)=>{
+		const ms = Number(item.biddingDate)
+		const d = new Date(ms);
 		const d_hour = prepend(d.getHours());
 		const d_minute = prepend(d.getMinutes());
 		const d_seconds = prepend(d.getSeconds());
-		history_elements.push(
-		<tr key={i}>
-			<td>{d.toLocaleDateString("en-US")}</td>
-			<td>{`${d_hour}:${d_minute}:${d_seconds}`}</td>
-			<td>{history[i].bidderName}</td>
-			<td>{history[i].biddingPrice}</td>
-		</tr>
-		);
-	}
+		return (
+			<tr key={index}>
+				<td>{d.toLocaleDateString("en-US")}</td>
+				<td>{`${d_hour}:${d_minute}:${d_seconds}`}</td>
+				<td>{item.bidderName}</td>
+				<td>{item.biddingPrice}</td>
+			</tr>
+		)
+	})
 	return (
 		<div className="modal fade" id="historyModal" tabIndex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
 			<div id="history-modal" className="modal-dialog modal-dialog-centered">
@@ -73,7 +72,6 @@ const Timer = (props)=>{
 
 const Gallery = (props)=>{
 	const pictures=props.pictures;
-	const picture_elements = []
 	const [main, setMain] = useState("")
 	useEffect(()=>{
 		setMain(pictures[0])
@@ -81,11 +79,16 @@ const Gallery = (props)=>{
 	const setImage = (pic)=>{
 		setMain(pic)
 	}
-	for(let i=0;i<pictures.length;i++){
-		picture_elements.push(
-			<img className="list-picture" src={pictures[i]} key={i} onClick={()=>setImage(pictures[i])}/>
+	const picture_elements = pictures.map((item,index)=>{
+		return (
+			<img 
+			className="list-picture" 
+			src={item} 
+			key={index} 
+			onClick={()=>setImage(item)}
+			/>
 		)
-	}
+	})
 	return (
 		<div id="gallery">
 			<div id="main-picture-wrapper">
@@ -104,21 +107,19 @@ const Gallery = (props)=>{
 
 const AuctionTop = (props)=>{
 	const history = getHistory(props.data);
-	let history_elements = []
-	for(let i=0;i<history.length && i<5;i++){
-		const timesince = Date.now()-Number(history[i].biddingDate);
+	let history_elements = history.map((item,index)=>{
+		const timesince = Date.now()-Number(item.biddingDate);
 		const timeformat = getDateSince(timesince);
-		history_elements.push(
-		<li className='topbidder-element' key={i}>
-			<div className='bidding-front'>
-				<span className='bidding-name'>{history[i].bidderName}</span>
-				<span className='bidding-date'>{timeformat}</span>
-			</div>
-			<span className='bidding-price'>{history[i].biddingPrice} Baht</span>
-		</li>
-		);
-	}
-	
+		return (
+			<li className='topbidder-element' key={index}>
+				<div className='bidding-front'>
+					<span className='bidding-name'>{item.bidderName}</span>
+					<span className='bidding-date'>{timeformat}</span>
+				</div>
+				<span className='bidding-price'>{item.biddingPrice} Baht</span>
+			</li>
+		)
+	})	
 	let historyError = false;
 	let errorMessage = "Something went wrong";
 	if(!props.isLoggedIn){
