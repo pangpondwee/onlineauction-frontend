2017279bb92eb5fdc04abd190747205080047c6a
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-10-12 22:05:03 +0700

    Edit nopage design

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,21 @@
+import pageNotFoundImg from '../pictures/page_not_found.jpg'
+import '../css/PageNotFound.css'
+
 const NoPage = () => {
-	return <h1>404</h1>
+  return (
+    <div className="page-not-found">
+      <div className="description-section">
+        <div className="description">
+          The page you requested could not be found
+        </div>
+      </div>
+      <img
+        src={pageNotFoundImg}
+        className="page-not-found-img"
+        alt="page not found"
+      />
+    </div>
+  )
 }
 
-export default NoPage;
+export default NoPage
