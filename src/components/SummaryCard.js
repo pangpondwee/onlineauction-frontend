ec86062890105fb932339650793bbd74721ac8cb
Nat <73571113+pangpondwee@users.noreply.github.com>
2022-10-15 18:41:35 +0700

    Edit css

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -1,15 +1,33 @@
 import '../css/Payment.css'
 
 const SummaryCard = (props) => {
+  const serviceFee = (props.bidPrice * 2) / 100
+  const total = props.bidPrice - serviceFee
   return (
     <div className="summary-card">
       <div className="summary-card-header">Summary</div>
-      <div className="summary-card-bidprice">
-        Bid Price {props.bidPrice} Baht
+      <div className="summary-card-price-section">
+        <div className="summary-card-bidprice">Bid Price</div>
+        <div className="summary-card-bidprice">{props.bidPrice} Baht</div>
       </div>
-      <div className="summary-card-total">
+      <div
+        className={`summary-card-price-section ${
+          props.isAuctioneer ? '' : 'hidden'
+        }`}
+      >
+        <div className="summary-card-bidprice">Service Fee 2%</div>
+        <div className="summary-card-bidprice">- {serviceFee} Baht</div>
+      </div>
+      <div>
         <hr />
-        Total {props.bidPrice} Baht
+        <div className="summary-card-price-section">
+          <div className="summary-card-price">Total</div>
+          {props.isAuctioneer ? (
+            <div className="summary-card-price">{total} Baht</div>
+          ) : (
+            <div className="summary-card-price">{props.bidPrice} Baht</div>
+          )}
+        </div>
       </div>
     </div>
   )
