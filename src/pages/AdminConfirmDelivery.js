30ff7ae42bda368393db5c8dd30c66d312093cc1
kong-wep <pakkapol.20100@gmail.com>
2022-11-11 20:43:42 +0700

    Fixed admin pictures

--- src/pages/AdminConfirmDelivery.js
+++ src/pages/AdminConfirmDelivery.js
@@ -31,7 +31,7 @@ const PopupConfirm = (props) => { // TODO use component popup confirm
         <h6>Shipping Company</h6>
         <p>{props.data.shippingCompany ? props.data.shippingCompany : "-"}</p>
         <h6>Package Picture</h6>
-        <img src={fetchPicture(props.data.packagePicture)} className="tracking-img" alt="tracking-img"/>
+        <img src={props.data.packagePicture} className="tracking-img" alt="tracking-img"/>
       </Modal.Body>
       <Modal.Footer>
         <button
